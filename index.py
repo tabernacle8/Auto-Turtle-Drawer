b414b6c5bc14b6983cf1fb850938e4b6aead0450
Carter Kosturos <tabernacle800@gmail.com>
2020-07-23 16:16:45 -0700

    Added Color

--- index.py
+++ index.py
@@ -4,10 +4,33 @@ from pygame import *
 import sys, random, math, fractions
 #import filereader
 
+     
+x_vals = []
+y_vals = []
 
 f = open("result.txt", "w")
+
+
+def resolveColor(color):
+    if(color =="green"):
+        return((0,255,0))
+    if(color =="red"):
+        return((255,0,0))
+    if(color =="yellow"):
+        return((255,255,0))
+    if(color =="blue"):
+        return((0,0,255))
+    if(color =="default" or color=="black"):
+        return((0,0,0))
+    
+def changeColor(newcolor):
+    pygame.display.set_caption("Color set to "+newcolor)
+    x_vals.append(newcolor)
+    y_vals.append(newcolor)
+
 # Define a main function
 def main():
+    changeColor("default")
      
     # Initialize the pygame module
     pygame.init()
@@ -21,11 +44,7 @@ def main():
      
     # Define a variable to control the main loop
     running = True
-     
 
-
-    x_vals = [0]
-    y_vals = [0]
     # Main loop
     timer = 0
     while running:
@@ -47,6 +66,25 @@ def main():
 
 
         for event in pygame.event.get():
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_g:
+                   changeColor("green")
+                   
+                if event.key == pygame.K_r:
+                    changeColor("red")
+                    
+                if event.key == pygame.K_y:
+                    changeColor("yellow")
+                   
+                if event.key == pygame.K_b:
+                    changeColor("blue")
+
+                if event.key == pygame.K_d:
+                    changeColor("black")
+                   
+
+
             if event.type==QUIT:
                 pygame.quit()
 
@@ -55,15 +93,23 @@ def main():
                 print("Final code:\n\n\n")
                 index = 1
                 while (index<len(x_vals)):
-                    f.write("turtle.penup();")
-                    f.write("turtle.goto("+str(x_vals[index]-170)+","+str((y_vals[index]-300)*-1)+");")
-                    f.write("turtle.pendown();")
-                    f.write("turtle.circle(1)\n")
+
+                    if(isinstance(x_vals[index], str)):
+                        f.write("turtle.color(\""+x_vals[index]+"\");")
+                    else:
+                        f.write("turtle.penup();")
+                        f.write("turtle.goto("+str(x_vals[index]-170)+","+str((y_vals[index]-300)*-1)+");")
+                        f.write("turtle.pendown();")
+                        f.write("turtle.circle(1)\n")
                     index = index+1
             
         index = 0
+        currentColor = (0,0,0)
         while (index<len(x_vals)):
-            pygame.draw.circle(screen, color, [x_vals[index],y_vals[index]], 1, 0)
+            if(not(isinstance(x_vals[index], str))):
+                pygame.draw.circle(screen, currentColor, [x_vals[index],y_vals[index]], 5, 0)
+            else:
+                currentColor = resolveColor(x_vals[index])
             index = index+1
 
         pygame.display.update()
--- test.py
+++ test.py
@@ -3,4 +3,5 @@ turtle.speed(100000000)
 
 #For testing your creations
 
+
 turtle.done()
